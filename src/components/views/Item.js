cecd1eccd85666144292b973d3b5571f91b76ed9
Bohdan <bodiasyd@gmail.com>
2020-03-29 20:59:55 +0300

    Added Item onPress event.

--- src/components/views/Item.js
+++ src/components/views/Item.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import { View, Text, Card } from "react-native-ui-lib";
 
 class Item extends Component {
-  render() {   
+  constructor() {
+    super();
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    console.log(123);
+  }
+
+  render() {
     return (
-      <Card padding-16 margin-8>
-        <Text>{this.props.key1}</Text>
-        <Text>{this.props.key2}</Text>
-        <Text>{this.props.key3}</Text>
+      <Card padding-16 margin-8 onPress={this.onPress}>
+        <Text part1>{this.props.key1}</Text>
+        <Text part2>{this.props.key2}</Text>
+        <Text part3>{this.props.key3}</Text>
       </Card>
     );
   }

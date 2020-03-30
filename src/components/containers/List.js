3956990f07f76f6c230714d6a1c178a60a1b0f7b
Bohdan <bodiasyd@gmail.com>
2020-03-30 08:24:55 +0300

    List improved.

--- src/components/containers/List.js
+++ src/components/containers/List.js
@@ -1,20 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 import { connect } from "react-redux";
-import { View } from "react-native-ui-lib";
+import { FlatList } from "react-native";
+import { View, ScrollBar } from "react-native-ui-lib";
 
 import Item from "src/components/views/Item";
 
-class List extends Component {
+class List extends PureComponent {
   getItems() {
     return this.props.items.map((itemData, index) => {
-      return <Item key={index} {...itemData} />;
+      return Object.assign({ key: `${index}` }, itemData);
     });
   }
 
   render() {
     let items = this.getItems();
-    
-    return <View bg-green10 padding-8>{items}</View>;
+
+    return (
+      <FlatList
+        renderItem={({ item }) => <Item {...item} />}
+        data={items}
+        // bg-green10
+        // padding-8
+        keyExtractor={item => item.key}
+      />
+    );
   }
 }
 

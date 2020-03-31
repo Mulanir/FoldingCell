680a540736321c122be297c27f887d64652da37c
Bohdan <bodiasyd@gmail.com>
2020-03-31 16:56:26 +0300

    Backwards animation added.

--- src/components/views/Item.js
+++ src/components/views/Item.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { StyleSheet, Animated } from "react-native";
+import { StyleSheet, Animated, Easing } from "react-native";
 import { Text, Card } from "react-native-ui-lib";
 
 import Panel, { AnimatedView } from "src/components/views/Panel";
@@ -10,38 +10,86 @@ class Item extends PureComponent {
   constructor() {
     super();
 
-    this.heightMain = 120;
-    this.heightSecond = 60;
-    this.duration = 400;
+    this.HEIGHT_MAIN = 120;
+    this.HEIGHT_SECOND = 60;
+    this.DURATION = 400;
+
     this.animate = this.animate.bind(this);
+    this.animateForw = this.animateForw.bind(this);
+    this.animateBack = this.animateBack.bind(this);
 
     this.state = {
+      inProgress: false,
+      nextAnimation: this.animateForw,
       mainValue: new Animated.Value(0),
       secondValue: new Animated.Value(0),
-      heightValue: new Animated.Value(this.heightMain),
+      heightValue: new Animated.Value(this.HEIGHT_MAIN),
     };
   }
 
   animate() {
+    if (this.state.inProgress) {
+      return;
+    }
+
+    this.setState({ inProgress: true });
+    this.state.nextAnimation();
+  }
+
+  animateForw() {
+    let callback = () =>
+      this.setState({
+        inProgress: false,
+        nextAnimation: this.animateBack,
+      });
     Animated.parallel([
       Animated.sequence([
         Animated.timing(this.state.mainValue, {
           toValue: 1,
-          duration: this.duration,
+          duration: this.DURATION,
           useNativeDriver: true,
         }),
         Animated.timing(this.state.secondValue, {
           toValue: 1,
-          duration: this.duration,
+          duration: this.DURATION,
           useNativeDriver: true,
         }),
       ]),
       Animated.timing(this.state.heightValue, {
-        toValue: this.heightMain * 2 + this.heightSecond,
-        duration: this.duration,
+        toValue: this.HEIGHT_MAIN * 2 + this.HEIGHT_SECOND,
+        duration: this.DURATION * 2,
+        easing: Easing.out(Easing.cubic),
         useNativeDriver: false,
       }),
-    ]).start();
+    ]).start(callback);
+  }
+
+  animateBack() {
+    let callback = () =>
+      this.setState({
+        inProgress: false,
+        nextAnimation: this.animateForw,
+      });
+    Animated.parallel([
+      Animated.sequence([
+        Animated.timing(this.state.secondValue, {
+          toValue: 0,
+          duration: this.DURATION,
+          useNativeDriver: true,
+        }),
+        Animated.timing(this.state.mainValue, {
+          toValue: 0,
+          duration: this.DURATION,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.timing(this.state.heightValue, {
+        toValue: this.HEIGHT_MAIN,
+        duration: this.DURATION * 2,
+        easing: Easing.in(Easing.cubic),
+        useNativeDriver: false,
+      }),
+    ]).start(callback);
   }
 
   render() {
@@ -50,14 +98,16 @@ class Item extends PureComponent {
         onPress={this.animate}
         margin-8
         activeOpacity={1}
+        borderRadius={0}
+        enableShadow={false}
         style={{
           height: this.state.heightValue,
         }}>
-        <AnimatedView height={this.heightMain} padding-16 bg-blue20>
+        <AnimatedView height={this.HEIGHT_MAIN} padding-16 bg-blue20>
           <Text part1>{this.props.key1}</Text>
         </AnimatedView>
         <Panel
-          height={this.heightMain}
+          height={this.HEIGHT_MAIN}
           animValue={this.state.mainValue}
           panelStyles={styles.mainItem}
           frontStyles={styles.mainItemFront}
@@ -66,7 +116,7 @@ class Item extends PureComponent {
           keyBack={this.props.key3}
           nextPanel={
             <Panel
-              height={this.heightSecond}
+              height={this.HEIGHT_SECOND}
               animValue={this.state.secondValue}
               panelStyles={styles.secondItem}
               frontStyles={styles.secondItemFront}

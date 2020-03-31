d2ca07d9dc972ed0f87de3a1773ff76f89b187a1
Bohdan <bodiasyd@gmail.com>
2020-03-31 17:36:24 +0300

    Prespective made 1500.

--- src/utils/transforms.js
+++ src/utils/transforms.js
@@ -6,7 +6,7 @@ export default function transform(ref, dx, y) {
   const rotate = transformUtil.rotateX(dx);
 
   transformUtil.origin(rotate, { x: 0, y, z: 0 });
-  transformUtil.applyPerspective(matrix, 1000);
+  transformUtil.applyPerspective(matrix, 1500);
   transformUtil.multiplyInto(matrix, matrix, rotate);
 
   ref.current.setNativeProps({
